c69d001833230c879f7f9c0df0e9ef35859323a1
devmohsan <mohsancode@gmail.com>
2026-04-06 20:34:34 +0500

    Switch to environment variables for Firebase config
    Refactor Firebase admin service account configuration to use environment variables instead of a JSON file.

--- index.js
+++ index.js
@@ -12,7 +12,20 @@ app.set('view engine', 'ejs'); // or pug, hbs, etc.
 app.set('views', path.join(__dirname, 'views'));
 
 const admin = require('firebase-admin');
-const serviceAccount = require('./credential.json');
+// const serviceAccount = require('./credential.json');
+const serviceAccount = {
+  type: process.env.FB_TYPE,
+  project_id: process.env.FB_PROJECT_ID,
+  private_key_id: process.env.FB_PRIVATE_KEY_ID,
+  private_key:process.env.FB_PRIVATE_KEY,
+  client_email: process.env.FB_CLIENT_EMAIL,
+  client_id: process.env.FB_CLIENT_ID,
+  auth_uri: process.env.FB_AUTH_URI,
+  token_uri: process.env.FB_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FB_AUTH_CERT_URL,
+  client_x509_cert_url: process.env.FB_CLIENT_CERT_URL,
+  universe_domain: process.env.FB_UNIVERSE_DOMAIN
+};
 
 if (!admin.apps.length) {
     admin.initializeApp({
